e0f1e947424dc8b479988f11025629821dcd6039
Alimerdan Rahimov <94564048+AliDevel@users.noreply.github.com>
2022-05-05 10:18:47 +0200

    Update DataSetAnalyzer.py
    Updated and added some comments

--- DataSetAnalyzer.py
+++ DataSetAnalyzer.py
@@ -3,26 +3,27 @@ import os
 import csv
 url={}
 useragent={}
-httpl={"3"}
+
 with open('user_agents.csv', newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
+         #Grouped unencrypted traffic by packages       
          if row["packageName"] not in url:
           if "http://" in row["url"]:
            url[row["packageName"]]=[]
            url[row["packageName"]].append(row["url"])
          elif "http://" in row["url"] :
           url[row["packageName"]].append(row["url"])
+         #Grouped user agents by  packages  
          if  row["user agent"] not in useragent:
            useragent[row["user agent"]]=[]
            useragent[row["user agent"]].append(row["packageName"])
          else:
            useragent[row["user agent"]].append(row["packageName"])       
       
-         if "http://" in row["url"]:
-           httpl.add(row["url"])
+  
        
-        
+       
         file = open('url.csv', 'a')
         writer = csv.writer(file)  
         header = ["packageName","url"] 
@@ -30,6 +31,8 @@ with open('user_agents.csv', newline='') as csvfile:
         for key, value in url.items():
             data=[key,value]
             writer.writerow(data)
+                
+                
         file = open('useragents1.csv', 'a')
         writer = csv.writer(file)  
         header = ["useragents","package"]
