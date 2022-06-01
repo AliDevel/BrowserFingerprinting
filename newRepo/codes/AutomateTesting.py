538edf56296db14a7fda3de902f7fb993246f99e
Alimerdan Rahimov <94564048+AliDevel@users.noreply.github.com>
2022-06-01 17:49:11 +0200

    Update AutomateTesting.py
    Monkeyrunner integration

--- newRepo/codes/AutomateTesting.py
+++ newRepo/codes/AutomateTesting.py
@@ -5,24 +5,24 @@ import re
 import os
 import csv
 import frida
-path = 'path/to/the/applications/folder'
-path1='path/to/the/tested/applications/folder'
+#Applications folder
+path = 'F:/automatization/app'
+#Tested Applications folder
+path1='F:/automatization/app3'
+#Path to monkeyrunner
+android_home=r'C:\Users\alime\AppData\Local\Android\Sdk\tools\bin\monkeyrunner.bat'
 
-
+#Connecting for a device
 def connect():
     client = AdbClient(host="127.0.0.1", port=5037) # Default is "127.0.0.1" and 5037
-
     devices = client.devices()
-
     if len(devices) == 0:
         print('No devices')
         quit()
-
     device = devices[0]
-
     print(f'Connected to {device}')
-
     return device, client
+
 
 def search_package_in_avd(device):
     command = device.shell('pm list packages -3 '+'|cut -f 2 -d '+':')
@@ -33,8 +33,18 @@ def search_package_in_avd(device):
         return ""
     else:
         return packages
+        
 def read_files():
+    input_list=[]
+    file = open("results3.csv")  
+    reader = csv.reader(file)
+    #gets all apk file names
     files = os.listdir(path)
+    
+    for row in reader:
+       if len(row)>0:
+        input_list.append(row)
+    file.close()    
     return files
 
 def install_package(package):
@@ -44,7 +54,10 @@ def install_package(package):
      return True
     except Exception as e:
      print("Error"+str(e))
-     os.remove(path+"/"+package)
+     try:
+      os.remove(path+"/"+package)
+     except Exception as e:
+      print("Error"+str(e))     
      return False
     
 def uninstall_package(device):
@@ -53,18 +66,19 @@ def uninstall_package(device):
         device.uninstall(package)
         print(package+" uninstalled")
 
-def file_init():
-    header = ["packageName","package","method","header","url","user agent"]
-    file = open('user_agents.csv', 'a')
+def file_open():
+    header = ["packageName","package","header","method","url","useragent"]
+    file = open('eval1.csv', 'a')
     writer = csv.writer(file)
-    # Use writerows() not writerow()
+   
     writer.writerow(header)    
     return file,writer
 
 def file_open1():
-    file = open('user_agents.csv', 'a')
+    file = open('eval1.csv', 'a')
     writer = csv.writer(file)
     return file,writer    
+    
 def add_rows(writer,data):
     writer.writerow(data)
 
@@ -75,13 +89,15 @@ def frida_instument():
     f_package=search_package_in_avd(device)[0];
     pid = device_frida.spawn([f_package])
     session = device_frida.attach(pid)
-    script = session.create_script(open("instrumentJs.js").read())
+    script = session.create_script(open("ev.js").read())
     script.on("message", on_message)
     script.load()
     device_frida.resume(pid)
-    time.sleep(5)
+    #running monkeyscript
+    os.system(android_home+' monkeyscript.py')
+    time.sleep(10)
   except Exception as e:
-    print("ERROR"+" "+str(e))
+    print("ERROR")
    
 
     
@@ -92,7 +108,7 @@ def on_message(message, data):
         payload = message['payload']  
         if 'Url' in payload:
             print("inFrida")
-            data=[payload['packageName'],package,payload['method'],payload['Header'],payload['Url'], payload['userAgent']]
+            data=[payload['packageName'],package,payload['method'],payload['Header'],payload['Url'],payload['userAgent']]
             file,writer=file_open1() 
             add_rows(writer,data)
             file.close()
@@ -104,19 +120,24 @@ f_package=None
 
 if __name__ == '__main__':
 
-    file,writer=file_init() 
+    file,writer=file_open() 
     device, client = connect()     
     uninstall_package(device)
+    # open up camera app
     apks=read_files() 
     for apk in apks:
+      if len(apk)>0:
        x=install_package(apk)
        package=apk
        if(x):
         frida_instument()
         uninstall_package(device)
-        os.replace(path+"/"+apk,path1+"/"+apk)       
+        try:
+         os.replace(path+"/"+apk,path1+"/"+apk)       
+        except Exception as e:
+             print("ERROR")
     file.close()
-  
+
 
 
         
